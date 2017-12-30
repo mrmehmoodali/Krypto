4ab93efee43a1e898fe09f0fe70222bf346ed526
Ali <abc@xyz.com>
2017-12-30 11:40:11 +0530

    First Commit

---
+++ app/src/main/java/com/example/ali/webapi/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.ali.webapi;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String API_URL = "https://koinex.in/api/ticker";
+
+    ProgressBar mProgressBar;
+    TextView mResponseView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mProgressBar = findViewById(R.id.progressBar);
+        mResponseView = findViewById(R.id.responseView);
+
+        new RetrieveFeedTask().execute();
+
+
+    }
+
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+        //private Exception exception;
+
+        protected void onPreExecute() {
+
+            mProgressBar.setVisibility(View.VISIBLE);
+            mResponseView.setText("");
+        }
+
+        protected String doInBackground(Void... urls) {
+            //String email = emailText.getText().toString();
+            // Do some validation here
+
+            try {
+                URL url = new URL(API_URL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            mProgressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+            mResponseView.setText(response);
+            // TODO: check this.exception
+            // TODO: do something with the feed
+
+//            try {
+//                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+//                String requestID = object.getString("requestId");
+//                int likelihood = object.getInt("likelihood");
+//                JSONArray photos = object.getJSONArray("photos");
+//                .
+//                .
+//                .
+//                .
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+}
+
