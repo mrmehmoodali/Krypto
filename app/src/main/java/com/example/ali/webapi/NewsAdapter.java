5e1fdc2cabbd23f8ada55dc66fd657bb13e7058a
Ali <abc@xyz.com>
2018-01-12 22:41:23 +0530

    News Added !!!

---
+++ app/src/main/java/com/example/ali/webapi/NewsAdapter.java
@@ -0,0 +1,92 @@
+package com.example.ali.webapi;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Ali on 1/12/2018.
+ */
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<HashMap<String,String>> values;
+
+    /*public TickerAdapter(Context context) {
+        this.context = context;
+    }*/
+
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public NewsAdapter(ArrayList<HashMap<String,String>> myDataset) {
+        values = myDataset;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public ImageView galleryImage;
+        public TextView author;
+        public TextView mtitle;
+        public TextView sdetails;
+        public TextView mtime;
+        public View layout;
+
+        public ViewHolder(View v) {
+            super(v);
+            layout      = v;
+            galleryImage    = v.findViewById(R.id.galleryImage);
+            author       = v.findViewById(R.id.author);
+            mtitle       = v.findViewById(R.id.title);
+            sdetails       = v.findViewById(R.id.sdetails);
+            mtime   = v.findViewById(R.id.time);
+        }
+    }
+
+
+    @Override
+    public NewsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        LayoutInflater inflater = LayoutInflater.from(
+                parent.getContext());
+        View view =
+                inflater.inflate(R.layout.news_list_items, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(NewsAdapter.ViewHolder holder, int position) {
+        HashMap<String,String> map = values.get(position);
+        //holder.author.setText(map.get(NewsTab.KEY_AUTHOR));
+        holder.mtitle.setText(map.get(NewsTab.KEY_TITLE));
+        holder.sdetails.setText(map.get(NewsTab.KEY_DESCRIPTION));
+        holder.mtime.setText(map.get(NewsTab.KEY_PUBLISHEDAT));
+        //holder.galleryImage.setImageResource(Integer.parseInt(map.get(NewsTab.KEY_URLTOIMAGE)));
+        if(map.get(NewsTab.KEY_URLTOIMAGE).length() < 5)
+        {
+            holder.galleryImage.setVisibility(View.GONE);
+        }else{
+            Picasso.with(holder.galleryImage.getContext())
+                    .load(map.get(NewsTab.KEY_URLTOIMAGE))
+                    //.resize(400, 300)
+                    .into(holder.galleryImage);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return values.size();
+    }
+}
--- app/src/main/java/com/example/ali/webapi/NewsTab.java
+++ app/src/main/java/com/example/ali/webapi/NewsTab.java
@@ -1,14 +1,27 @@
 package com.example.ali.webapi;
 
 import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Ali on 1/4/2018.
@@ -16,19 +29,88 @@ import android.webkit.WebViewClient;
 
 public class NewsTab extends Fragment {
 
+    String API_KEY = "f64e84a553ae44ba8bdb9e79919eb545"; // ### YOUE NEWS API HERE ###
+    String NEWS_SOURCE = "bbc-news";
+    RecyclerView listNews;
+    ProgressBar loader;
+    private RecyclerView.LayoutManager nLayoutManager;
+    private RecyclerView.Adapter nAdapter;
+
+    ArrayList<HashMap<String, String>> dataList = new ArrayList<>();
+    static final String KEY_AUTHOR = "author";
+    static final String KEY_TITLE = "title";
+    static final String KEY_DESCRIPTION = "description";
+    static final String KEY_URL = "url";
+    static final String KEY_URLTOIMAGE = "urlToImage";
+    static final String KEY_PUBLISHEDAT = "publishedAt";
+
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.web_tab,container,false);
-        WebView webview = v.findViewById(R.id.webview);
-        webview.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return false;
-            }
-        });
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.loadUrl("https://koinex.in");
+        View v =inflater.inflate(R.layout.news_tab,container,false);
+        dataList = new ArrayList<>();
+        loader = v.findViewById(R.id.progressBar);
+        listNews = v.findViewById(R.id.news_recycler_view);
+        new DownloadNews().execute();
         return v;
+    }
+
+    private class DownloadNews extends AsyncTask<Void, Void, String> {
+
+        //private Exception exception;
+
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+
+        }
+
+        protected String doInBackground(Void... urls) {
+            //String email = emailText.getText().toString();
+            // Do some validation here
+            HttpHandler sh = new HttpHandler();
+
+            // Making a request to url and getting response
+            String xml = sh.makeServiceCall
+                    ("https://newsapi.org/v2/top-headlines?q=cryptocurrency&sortBy=popularity&apiKey="+API_KEY);
+
+
+            Log.e(TAG, "Response from news: " + xml);
+
+            return xml;
+        }
+
+        @SuppressLint("NewApi")
+        protected void onPostExecute(String xml) {
+
+            try {
+                JSONObject jsonResponse = new JSONObject(xml);
+                JSONArray jsonArray = jsonResponse.optJSONArray("articles");
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    HashMap<String, String> map = new HashMap<>();
+                    map.put(KEY_AUTHOR, jsonObject.optString(KEY_AUTHOR));
+                    map.put(KEY_TITLE, jsonObject.optString(KEY_TITLE));
+                    map.put(KEY_DESCRIPTION, jsonObject.optString(KEY_DESCRIPTION));
+                    map.put(KEY_URL, jsonObject.optString(KEY_URL));
+                    map.put(KEY_URLTOIMAGE, jsonObject.optString(KEY_URLTOIMAGE));
+                    map.put(KEY_PUBLISHEDAT, jsonObject.optString(KEY_PUBLISHEDAT));
+                    dataList.add(map);
+                }
+                //listNews.setHasFixedSize(true);
+                // use a linear layout manager
+                nLayoutManager = new LinearLayoutManager(getActivity());
+                listNews.setLayoutManager(nLayoutManager);
+                nAdapter = new NewsAdapter(dataList);
+
+                listNews.addItemDecoration
+                        (new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+                listNews.setAdapter(nAdapter);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
