cb2093bdb652825c75fae13b8aa747b2ea55d4fd
Ali <abc@xyz.com>
2018-01-14 21:22:24 +0530

    Added card to News

--- app/src/main/java/com/example/ali/webapi/NewsTab.java
+++ app/src/main/java/com/example/ali/webapi/NewsTab.java
@@ -5,7 +5,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -104,8 +103,8 @@ public class NewsTab extends Fragment {
                 listNews.setLayoutManager(nLayoutManager);
                 nAdapter = new NewsAdapter(dataList);
 
-                listNews.addItemDecoration
-                        (new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+                //listNews.addItemDecoration
+                 //       (new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
                 listNews.setAdapter(nAdapter);
 
             } catch (JSONException e) {
